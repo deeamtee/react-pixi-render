cb1f4c801f04fcd58d6a95def9f18db76a5175bc
Sergey Konstantinov <sergey.konstantinov@raiffeisen.ru>
2022-09-20 13:21:33 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="example">
+      <img src={logo} alt="logo" />
+      <p>Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
